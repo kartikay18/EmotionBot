11460ca43845b04df204b826ffc53f5307869fe8
Kartikay <kartikay.goyle@hotmail.com>
2016-11-12 15:32:43 -0500

    Update MessagesController.cs

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -17,58 +17,6 @@ using Microsoft.ProjectOxford.Face.Contract;
 
 namespace Bot_Application1
 {
-
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            // Create grabber. 
-            FrameGrabber<Face[]> grabber = new FrameGrabber<Face[]>();
-
-            // Create Face API Client.
-            FaceServiceClient faceClient = new FaceServiceClient("b84106a40d9f4bf789f458a9ee3fc7e2");
-
-            // Set up a listener for when we acquire a new frame.
-            grabber.NewFrameProvided += (s, e) =>
-            {
-                Console.WriteLine("New frame acquired at {0}", e.Frame.Metadata.Timestamp);
-            };
-
-            // Set up Face API call.
-            grabber.AnalysisFunction = async frame =>
-            {
-                Console.WriteLine("Submitting frame acquired at {0}", frame.Metadata.Timestamp);
-                // Encode image and submit to Face API. 
-                return await faceClient.DetectAsync(frame.Image.ToMemoryStream(".jpg"));
-            };
-
-            // Set up a listener for when we receive a new result from an API call. 
-            grabber.NewResultAvailable += (s, e) =>
-            {
-                if (e.TimedOut)
-                    Console.WriteLine("API call timed out.");
-                else if (e.Exception != null)
-                    Console.WriteLine("API call threw an exception.");
-                else
-                    Console.WriteLine("New result received for frame acquired at {0}. {1} faces detected", e.Frame.Metadata.Timestamp, e.Analysis.Length);
-            };
-
-            // Tell grabber when to call API.
-            // See also TriggerAnalysisOnPredicate
-            grabber.TriggerAnalysisOnInterval(TimeSpan.FromMilliseconds(3000));
-
-            // Start running in the background.
-            grabber.StartProcessingCameraAsync().Wait();
-
-            // Wait for keypress to stop
-            Console.WriteLine("Press any key to stop...");
-            Console.ReadKey();
-
-            // Stop, blocking until done.
-            grabber.StopProcessingAsync().Wait();
-        }
-    }
-
     [BotAuthentication]
     public class MessagesController : ApiController
     {
